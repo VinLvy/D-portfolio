8ae8ad031be30042d5dd52da60ba9ef08b23f179
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-03-24 10:28:45 +0700

    Navbar 1.0

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from " @@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar /> {/* Navbar akan muncul di semua halaman */}
+        <main className="container mx-auto p-4">{children}</main>
       </body>
     </html>
   );
