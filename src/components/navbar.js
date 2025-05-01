40bf6fe9e30f52d13d46ec0c469824e2e1101c7a
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-05-01 10:04:35 +0700

    Navbar 1.9

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,21 +10,21 @@ export default function Navbar() {
   return (
     <nav className="fixed bottom-0 sm:top-0 sm:bottom-auto w-full z-50 flex items-center justify-between sm:px-6 sm:py-3 px-4 py-2">
       {/* Kiri (hanya desktop) */}
-      <div className="hidden sm:block text-xl font-bold tracking-widest font-mono">
+      {/* <div className="hidden sm:block text-xl font-bold tracking-widest font-mono">
         D PORTFOLIO
-      </div>
+      </div> */}
 
       {/* Tengah */}
-      <div className="mt-2 bg-gray-900/50 backdrop-blur-lg shadow-md rounded-full flex px-4 py-2 sm:px-6 sm:py-3 space-x-2 sm:space-x-4 max-w-full mx-auto mb-3">
+      <div className="mt-2 bg-gray-900/50 backdrop-blur-lg shadow-md rounded-full flex px-4 py-2 sm:px-6 sm:py-3 space-x-2 sm:space-x-4 max-w-full mx-auto mb-5">
         <NavItem href="/" icon={<Home size={20} />} label="Home" active={pathname === "/"} />
         <NavItem href="/about" icon={<User size={20} />} label="About" active={pathname === "/about"} />
         <NavItem href="/projects" icon={<Briefcase size={20} />} label="Projects" active={pathname === "/projects"} />
       </div>
 
       {/* Kanan (hanya desktop) */}
-      <div className="hidden sm:block text-xl font-bold tracking-widest font-mono">
+      {/* <div className="hidden sm:block text-xl font-bold tracking-widest font-mono">
         DAVIN P F
-      </div>
+      </div> */}
     </nav>
   );
 }
