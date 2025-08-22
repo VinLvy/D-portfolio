3258a16b1f87d7ee0a3f152cac1dfbfc5d799943
VinLvy <123059312+VinLvy@users.noreply.github.com>
2025-08-22 18:17:06 +0700

    Vercel Analytics
    Installed @vercel/analytics package and integrated its Analytics component into the app layout to enable analytics tracking. Updated package.json and package-lock.json to include the new dependency.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from " @@/components/navbar";
 import CursorBlur from "../components/CursorBlur";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main className="w-full">{children}</main>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
